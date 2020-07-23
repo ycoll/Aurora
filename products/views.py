98ddec9b080f2ec7196e011803240ea378fed7e2
ycoll <yascoll@gmail.com>
2020-07-23 13:19:47 +0000

    Made search bar functional

--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from.models import Product
 # Create your views here.
 
@@ -6,9 +8,21 @@ from.models import Product
 def all_products(request):
 
     products = Product.objects.all()
+    search = None
+
+    if request.GET:
+        if 's' in request.GET:
+            search = request.GET['s']
+            if not search:
+                messages.error(request, "No search entered")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=search) | Q(description__icontains=search)
+            products = products.filter(queries)
 
     context = {
         'products': products,
+        'search_term': search,
     }
 
     return render(request, 'products/products.html', context)
